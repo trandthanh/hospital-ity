e30e5a7ae811484120d3767ad07f573092afead8
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-01-18 18:36:53 +0100

    front / first part of home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @hospitals = Hospital.all.map{ |h| h.hospital_name }
   end
 end
