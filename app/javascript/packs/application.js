27ee3e8cf22eb32a3ebb8d24e87285626c9f824c
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-01-31 16:29:43 +0100

    back / unavailable booking for selected dates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,12 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import "../plugins/flatpickr"
+import { toggleDateInputs } from '../plugins/flatpickr';
 
 initMapbox();
+toggleDateInputs();
 
+
+
+const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
+console.log(unvailableDates)
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,40 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {})
+
+// flatpickr(".datepicker", {})
+
+const toggleDateInputs = function() {
+  const arrivalInput = document.getElementById('booking_arrival');
+  const departureInput = document.getElementById('booking_departure');
+
+  if (arrivalInput && departureInput) {
+    const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
+
+    flatpickr(arrivalInput, {
+    enableTime: false,
+    minDate: 'today',
+    dateFormat: 'Y-m-d',
+    disable: unvailableDates,
+    onChange: function(selectedDates, selectedDate) {
+      if (selectedDate === '') {
+        departureInput.disabled = true;
+      }
+      let minDate = selectedDates[0];
+      minDate.setDate(minDate.getDate() + 1);
+      departureCalendar.set('minDate', minDate);
+      departureInput.disabled = false;
+    }
+  });
+    const departureCalendar =
+      flatpickr(departureInput, {
+        enableTime: false,
+        dateFormat: 'Y-m-d',
+        disable: unvailableDates
+      });
+  }
+};
+
+export { toggleDateInputs }
 
 
