6610f90812bc7869638a8a1d26787bc2b6d06643
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-04-10 09:19:29 +0200

    Back & Front / Working on Hospital Admin Dashboard

--- app/controllers/unapproved_users_controller.rb
+++ app/controllers/unapproved_users_controller.rb
@@ -3,6 +3,8 @@ class UnapprovedUsersController < ApplicationController
   def unapprovedlist
     authorize :unapproved_user, :unapprovedlist?
 
+    @user = current_user
+
     if params[:approved] == "false"
       @users = User.where(approved: false, hospital_id: current_user.hospital_id)
       # @users = policy_scope(User).where(approved: false)
